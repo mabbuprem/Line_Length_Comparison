b32723bb3f3abf2f7a4724ac1a2c80154f1a39f3
mabbuprem <mabbupremkumar@gmail.com>
2022-02-14 18:49:46 +0530

    equality of 2 lines

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace Line_Length
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter Co-ordinates of 1st Line");
             double line1Length = LineLength();
-            Console.WriteLine($"The length of line is {line1Length}");
+            Console.WriteLine("Enter Co-ordinates of 2nd Line");
+            double line2Length = LineLength();
+            string result = line1Length.Equals(line2Length) == true ? "Two lines are equal" : "Two lines are not equal";
+            Console.WriteLine(result);
         }
         public static double LineLength()
         {
@@ -23,8 +27,8 @@ namespace Line_Length
             double x12 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter 2nd Y co-ordinate of Line: ");
             double y12 = Convert.ToDouble(Console.ReadLine());
-            double lengthOfLine1 = Math.Pow(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2), 0.5);
-            return lengthOfLine1;
+            double lengthOfLine = Math.Pow(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2), 0.5);
+            return lengthOfLine;
         }
     }
 }
